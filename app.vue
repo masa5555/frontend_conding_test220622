e9ddfd99ddd9b1a5e95c3407768eac678ec326f5
masa5555 <masa57887@gmail.com>
2022-06-22 14:05:41 +0000

    Add RESAS API Wrapper

--- app.vue
+++ app.vue
@@ -2,4 +2,14 @@
   <div>Index</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { onMounted } from 'vue'
+  // import axios from 'axios'
+
+  onMounted(async () => {
+    // const res1 = await axios.get(`/api/getPrefectureList`)
+    // console.warn(res1)
+    // const res2 = await axios.get(`/api/getPrefecturePopulation?prefCode=${20}`)
+    // console.warn(res2)
+  })
+</script>
---
+++ server/api/getPrefecturePopulation.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import { defineEventHandler, useQuery } from 'h3'
+
+const { API_KEY } = process.env
+
+const getPrefecturePopulation = defineEventHandler(async (event) => {
+  if (API_KEY === undefined) {
+    throw new Error(`API_KEY is required`)
+  }
+  const { prefCode } = useQuery(event)
+  if (!prefCode) {
+    throw new Error(`required prefCode`)
+  }
+  const cityCode = '-' // all of prefecture
+  const { data } = await axios.get(
+    `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCpde=${cityCode}&prefCode=${prefCode}`,
+    { headers: { 'X-API-KEY': API_KEY } }
+  )
+
+  return {
+    data: data.result.data[0].data, //総人口
+  }
+})
+
+export default getPrefecturePopulation
